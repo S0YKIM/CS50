ebcd42075eb77eecabfb7c0ebb09059217f18bde
SOYEON KIM <soyeonkim.9683@gmail.com>
2021-02-05 20:02:12 +0900

    Create grade.for.c

---
+++ WEEK3/grade.for.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+const int number_of_scores = 9;
+const int scores[number_of_scores] = {95, 90, 85, 80, 75, 70, 65, 60, 0};
+
+const int number_of_grades = 9;
+const char *grades[number_of_grades] = {"A+", "A", "B+", "B", "C+", "C", "D+", "D", "F"};
+
+void get_grade(int k);
+
+int main(void)
+{
+    printf("[학점 프로그램]\n");
+    printf("종료를 원하면 '-1'를 입력\n");
+    printf("[학점 테이블]\n");
+    printf("점수: 95  90  85  80  75  70  65  60  0\n");
+    printf("학점: A+  A   B+  B   C+  C   D+  D   F\n");
+    
+    
+        while (1)
+        {
+        int n;
+        printf("성적을 입력하세요 (0~100) ");
+        scanf("%d", &n);
+        
+        if (n == -1)
+        {
+        printf("학점 프로그램을 종료합니다.\n");
+        break;
+        }
+        else if (n < 0 || n > 100) // 유효성 검사
+        {
+            printf("성적을 올바르게 입력하세요 (0 ~ 100)\n");
+            continue;
+        }
+        else 
+        {
+            get_grade(n); 
+        }
+    }
+}
+    
+    // 입력받은 성적을 바탕으로 학점 구하기
+    void get_grade (int k)
+    {
+        for (int i=0; i < number_of_scores; i++)
+        {
+            if (k < scores[i] & k >= scores[i+1])
+            {
+                printf("학점은 %s 입니다.\n", grades[i+1]);
+            }
+            else if (k >= 95)
+            {
+                printf("학점은 A+ 입니다.\n");
+                break;
+            }
+        }
+    }
