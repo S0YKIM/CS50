ed80779ea8497f5b69015f7a3fb1c051b3b24206
SOYEON KIM <soyeonkim.9683@gmail.com>
2021-02-11 23:10:21 +0900

    Create pointer.c

---
+++ WEEK5/pointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = "EMMA";
+    
+    // Print the memory addresses
+    printf("%p\n", s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
+    
+    // Print the string
+    printf("%s\n", s);
+    printf("%c\n", *s);
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
+    printf("%c\n", *(s+3));
+}
